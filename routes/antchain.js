39de6635ce16d6a449359b6fe22fc686ed688eab
hnustfeng <hnustwhf@163.com>
2023-03-30 13:06:03 +0800

    async

--- routes/antchain.js
+++ routes/antchain.js
@@ -20,8 +20,8 @@ const accountKey = fs.readFileSync("./src/user.pem", { encoding: "utf8" });
 const accountPassword = "123abc";  //自定义的 user.pem 密码
 
 const keyInfo = Chain.utils.getKeyInfo(accountKey, accountPassword);
-console.log("pub:",keyInfo.publicKey);
-console.log("pri:",keyInfo.privateKey);
+// console.log("pub:",keyInfo.publicKey);
+// console.log("pri:",keyInfo.privateKey);
 
 const passphrase = "123abc"; //自定义的 client.key 密码
 
@@ -34,14 +34,14 @@ router.post('/aaa', function(req, res, next) {
         }
         conn.release();
     })
-   console.log("pri:",keyInfo.privateKey.toString('hex'));
+//    console.log("pri:",keyInfo.privateKey.toString('hex'));
    const a = Math.sqrt(req.body.contractName)
     res.send({"a":a});
 
 });
 
 
-router.post('/createAccount',function(req,res,next){
+router.post('/createAccount',async function(req,res,next){
     //配置选项
     let opt = {
         host: '121.40.249.148',    //目标区块链网络节点的 IP
@@ -82,10 +82,10 @@ router.post('/createAccount',function(req,res,next){
                     res.end;
                 }else{
                     var  sql = "insert into user (name,pubkey,prikey,phonenumber,userhash) values ('" + username + "','" + newKey.privateKey.toString('hex') + "','" + newKey.publicKey.toString('hex') + "','" + req.body.phoneNumber+"','" + Chain.utils.getHash(username) + "')";
-                    console.log(sql);
+                    // console.log(sql);
                     conn.query(sql,function(err,result){
                         if(err){
-                                console.log('[insert ERROR] - ',err.message);
+                                // console.log('[insert ERROR] - ',err.message);
                                 res.send({"code":400,"message":"数据库操作失败！"});
                                 res.end;
                         }else{
@@ -103,7 +103,7 @@ router.post('/createAccount',function(req,res,next){
     })
 })
 
-router.post('/balanceOf',function(req,res,next){
+router.post('/balanceOf',async function(req,res,next){
     let opt = {
         host: '121.40.249.148',    //目标区块链网络节点的 IP
         port: 18130,          //端口号
@@ -147,7 +147,7 @@ router.post('/balanceOf',function(req,res,next){
     })
 })
 
-router.post('/getBaseURL',function(req,res,next){
+router.post('/getBaseURL',async function(req,res,next){
     let opt = {
         host: '121.40.249.148',    //目标区块链网络节点的 IP
         port: 18130,          //端口号
@@ -191,7 +191,7 @@ router.post('/getBaseURL',function(req,res,next){
     return;
 })
 
-router.post('/getName',function(req,res,next){
+router.post('/getName',async function(req,res,next){
     let opt = {
         host: '121.40.249.148',    //目标区块链网络节点的 IP
         port: 18130,          //端口号
@@ -234,7 +234,7 @@ router.post('/getName',function(req,res,next){
     })
 })
 
-router.post('/totalSupply',function(req,res,next){
+router.post('/totalSupply',async function(req,res,next){
     let opt = {
         host: '121.40.249.148',    //目标区块链网络节点的 IP
         port: 18130,          //端口号
@@ -277,7 +277,7 @@ router.post('/totalSupply',function(req,res,next){
     })
 })
 
-router.post('/getTokenURL',function(req,res,next){
+router.post('/getTokenURL',async function(req,res,next){
     let opt = {
         host: '121.40.249.148',    //目标区块链网络节点的 IP
         port: 18130,          //端口号
@@ -311,18 +311,18 @@ router.post('/getTokenURL',function(req,res,next){
     // })
     myContract.getTokenURL(req.body.tokenId,{from:'test'},(err,output,data)=>{
         if(err){
-            console.log(data);
+            // console.log(data);
             res.send({"code":400,"message":"查询失败！","data":err.output,"TxHash":data.txhash})
             res.end
         }else{
-            console.log(data);
+            // console.log(data);
             res.send({"code":200,"message":"查询成功！","data":output,"TxHash":data.txhash})
             res.end
         }
     })
 })
 
-router.post('/ownerOf',function(req,res,next){
+router.post('/ownerOf',async function(req,res,next){
     let opt = {
         host: '121.40.249.148',    //目标区块链网络节点的 IP
         port: 18130,          //端口号
@@ -365,17 +365,17 @@ router.post('/ownerOf',function(req,res,next){
     })
 })
 
-router.post('/approve',function(req,res,next){
+router.post('/approve',async function(req,res,next){
     pool.getConnection(function(err,conn){
         if(err){
             res.send({"code":400,"message":"数据库连接失败！","data":err});
             res.end
         }else{
             let sql = "select * from user where phonenumber = '" + req.body.phoneNumber + "'";
-            console.log(sql);
+            // console.log(sql);
             conn.query(sql,function(err,result){
                 if(err){
-                    console.log('[SELECT ERROR] - ',err);
+                    // console.log('[SELECT ERROR] - ',err);
                     res.send({"code":400,"message":"数据库查询错误！","data":err})
                     res.end
                     }
@@ -431,7 +431,7 @@ router.post('/approve',function(req,res,next){
     })
 })
 
-router.post('/mint',function(req,res,next){
+router.post('/mint',async function(req,res,next){
     pool.getConnection(function(err,conn){
         if(err){
             res.send({"code":400,"message":"数据库连接失败！","data":err})
@@ -474,9 +474,9 @@ router.post('/mint',function(req,res,next){
                     res.send({"code":400,"message":"mint失败！","data":err.output})
                     res.end
                 }else{
-                    console.log(data.receipt.log_entry[1].log_data);
-                    console.log(data.receipt.log_entry[1].log_data[1].toNumber());
-                    console.log(data.receipt.log_entry[1].log_data[2].toNumber());
+                    // console.log(data.receipt.log_entry[1].log_data);
+                    // console.log(data.receipt.log_entry[1].log_data[1].toNumber());
+                    // console.log(data.receipt.log_entry[1].log_data[2].toNumber());
                     let tokenId = [];
                     for(i = 0;i<data.receipt.log_entry[1].log_data[1].toNumber();i++){
                         tokenId.push(data.receipt.log_entry[1].log_data[2].toNumber() - i)
@@ -490,7 +490,7 @@ router.post('/mint',function(req,res,next){
 })
 })
 
-router.post('/setBaseURL',function(req,res,next){
+router.post('/setBaseURL',async function(req,res,next){
     let opt = {
         host: '121.40.249.148',    //目标区块链网络节点的 IP
         port: 18130,          //端口号
@@ -526,7 +526,7 @@ router.post('/setBaseURL',function(req,res,next){
         if(err){
             res.send({"code":400,"message":"修改失败！","data":err.output})
         }else{
-            console.log(data.txhash);
+            // console.log(data.txhash);
             res.send({"code":200,"message":"修改成功！","data":output,"TxHash":data.txhash})
             res.end
         }
@@ -534,22 +534,22 @@ router.post('/setBaseURL',function(req,res,next){
 })
 const contractId = 'c6ec36fb19cfdd8db7c998df1b9c69f021145777f84b19ec2d4d5c2f1309b696'
 
-router.post('/transfer',function(req,res,next){
+router.post('/transfer',async function(req,res,next){
     pool.getConnection(function(err,conn){
         if(err){
             res.send({"code":400,"message":"数据库连接失败！","data":err})
         }else{
             let sql = "select * from user where phonenumber = '" + req.body.phoneNumber + "'";
-            console.log(sql);
+            // console.log(sql);
             conn.query(sql,function(err,result){
                 if(err){
-                    console.log('[SELECT ERROR] - ',err);
+                    // console.log('[SELECT ERROR] - ',err);
                     res.send({"code":400,"message":"数据库查询错误！","data":err})
                     }
                     if(!result[0]){
                         res.send({"code":400,"message":"用户手机号不存在！","data":""})
                     }else{
-                        console.log(result[0].prikey);
+                        // console.log(result[0].prikey);
                         let opt = {
                             host: '121.40.249.148',    //目标区块链网络节点的 IP
                             port: 18130,          //端口号
@@ -586,7 +586,7 @@ router.post('/transfer',function(req,res,next){
                             if(err){
                                 res.send({"code":400,"message":"交易失败！","data":err.output,"TxHash":data.txhash})
                             }else{
-                                console.log(data.receipt.log_entry);
+                                // console.log(data.receipt.log_entry);
                                 res.send({"code":200,"message":"交易成功！","data":output,"TxHash":data.txhash})
                             }
                         })
@@ -597,7 +597,7 @@ router.post('/transfer',function(req,res,next){
     })
 })
 
-router.post('/newContract', function(req, res, next) {
+router.post('/newContract', async function(req, res, next) {
     // const contract = fs.readFileSync('./CreditManager.sol', {encoding: 'ascii'})
     // // 第二个参数设定为"1"，会开启编译优化 optimiser
     // const output = solc.compile(contract, 1)
@@ -616,7 +616,7 @@ router.post('/newContract', function(req, res, next) {
         userRecoverPrivateKey: keyInfo.privateKey,
         passphrase: passphrase
     }
-    console.log("22");
+    // console.log("22");
     //初始化一个连接实例
     const chain = Chain(opt)
     const contract = fs.readFileSync('./src/contract.sol', {encoding: 'ascii'});
@@ -626,17 +626,17 @@ router.post('/newContract', function(req, res, next) {
     const abi = JSON.parse(output.contracts[':AssetSample'].interface);
     const bytecode = output.contracts[':AssetSample'].bytecode;
     // 带上时间戳，防止合约名计算哈希后已存在
-    console.log("33333");
+    // console.log("33333");
     const contractName = req.body.contractName
     // 初始化一个合约实例
     let myContract = chain.ctr.contract(contractName, abi) 
     // 部署合约，可传递初始化函数需要的参数
-    console.log(req.body.contractName, req.body.contractBaseURL);
+    // console.log(req.body.contractName, req.body.contractBaseURL);
     myContract.new(bytecode,{
       from: 'test',
       // parameters: [param1, param2]
     }, (err, contract, data) => {
-        console.log("111111111");
+        // console.log("111111111");
       if (err){
         res.send({"code":400,"message":"合约部署失败！","err_message":err})
         res.end
@@ -663,15 +663,15 @@ router.post('/newContract', function(req, res, next) {
 });
 
 router.post('/test', function(req, res, next) {
-    console.log(req.body.phoneNumber);
+    // console.log(req.body.phoneNumber);
     pool.getConnection(function(err,conn){
         if(err){
-            console.log("lianjieshbai")
+            // console.log("lianjieshbai")
         }else{
             let sql = "select * from user";
             res.send(sql);
             return;
-            console.log("ssss");
+            // console.log("ssss");
             conn.query(sql,function(err,result){
                 if(err){
                     res.send({"code":300,"message":"交易shibai"});
